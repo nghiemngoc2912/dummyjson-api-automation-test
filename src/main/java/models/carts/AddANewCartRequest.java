8be52dfe5d0cac914d22b97f12df567b9430e3b6
nghiemngoc2912 <nghiemngoc2912@gmail.com>
2026-02-24 20:36:13 +0700

    add lombok library

--- src/main/java/models/carts/AddANewCartRequest.java
+++ src/main/java/models/carts/AddANewCartRequest.java
@@ -1,59 +1,22 @@
 package models.carts;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
 public class AddANewCartRequest {
     private String userId;
     private List<Product> products;
 
-    public AddANewCartRequest() {
-    }
-
-    public AddANewCartRequest(String userId, List<Product> products) {
-        this.userId = userId;
-        this.products = products;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
+    @AllArgsConstructor
+    @Data
     public static class Product{
         private String id;
         private String quantity;
-
-        public Product(String id, String quantity) {
-            this.id = id;
-            this.quantity = quantity;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(String quantity) {
-            this.quantity = quantity;
-        }
     }
 
 }
